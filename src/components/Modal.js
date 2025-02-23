4137785f8a4d7618263522f28af69aa95fe84cd7
mousepotato03 <seongjin2@kau.kr>
2025-02-23 21:03:28 +0900

    first commit

---
+++ src/components/Modal.js
@@ -0,0 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './Modal.module.css';
+
+function Modal({ children }) {
+    const navigate = useNavigate()
+
+    function closeHandler() {
+        navigate('..'); //pop
+    };
+
+    return (
+        <>
+            <div className={styles.backdrop} onClick={closeHandler} />
+            <dialog open className={styles.modal}>{children}</dialog>
+        </>
+    );
+}
+
+export default Modal;
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost, { action as newPostAction } from './routes/NewPost';
+import RootLayout from './routes/RootLayout';
+import Posts, { loader as postsLoader } from './routes/Posts';
+import PostDetails, { loader as postDetailsLoader } from './routes/PostDetails';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <RootLayout />, children: [
+      {
+        path: '/',
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:postId', element: <PostDetails />, loader: postDetailsLoader }
+        ]
+      },
+
+    ],
+  },
+
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
